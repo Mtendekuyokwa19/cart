f7e04b95f01b09f5c383f5daa4c1954c0db7e991
mtende <mtendekuyokwa19@gmail.com>
2024-11-01 23:47:56 +0200

    making the home page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
-
+import { Link, Outlet } from 'react-router-dom'
+import home from './home.jpg'
+import { Carticon } from '../svg'
 export default function Home() {
   return (
-<div>
+<div style={{
+background:` linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${home})`
 
-    <div>Home</div>
-<h1>Hey</h1>
+}} className='text-white flex justify-center items-center  h-full'>
+<div className='flex gap-3 flex-col items-center px-24 '>
 
+<h1 className='text-9xl font-bold font-serif'>BUILIDING CULTURE</h1>
+
+<p className='text-base'>Fashion in Africa is a vivid and dynamic expression of the continent's rich cultural tapestry. It reflects a deep connection to heritage, with traditional garments such as kente cloth from Ghana, shweshwe fabric from South Africa, and the boubou from West Africa showcasing unique identities and histories.</p>
+<div className=' flex gap-4'>
+<Link to={"shop"}>
+
+<button className='px-6 py-4 shadow-black flex gap-2 bg-lime-700 hover:bg-lime-600 justify-center items-center rounded'>
+<Carticon/>
+<p>Visit Shop</p>
+
+</button>
+
+</Link>
+<Link to={"www.github.com"}>
+<button>
+
+<p className='px-6 py-4 flex gap-2 outline outline-2 w-40 justify-center items-center rounded' >Github</p>
+</button>
+
+</Link>
+</div>
+</div>
 </div>
   )
 }
--- src/nav.tsx
+++ src/nav.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Nav() {
   return (
-<>
+<div className=' text-black h-screen w-screen overflow-x-hidden'  >
 
-  <nav className='flex '>
-   <div>
+  <nav className='sm:flex sm:justify-between sm:px-4 bg-gray-200 rounded-b'>
+   <div className='flex p-4'>
    <button><Link to="/">Jewdisha</Link></button>
 
 </div>
-   <div>
+   <div className='flex justify-evenly list-none items-center gap-44'>
 
 <li><Link to="/">Home</Link></li>
 <li><button><Link to="shop">Shop</Link></button></li>
-<li><button><Link to="cart">cart</Link></button></li>
+<li><button><Link to="cart">github</Link></button></li>
 </div>
-   <div>
-<li><button><Link to="cart">Github</Link></button></li>
+   <div className='items-center list-none justify-center flex p-4'>
+<li><button><Link to="cart">cart</Link></button></li>
 </div>
 
 </nav>
 
 <Outlet/>
-</>
+</div>
   )
 }
 
